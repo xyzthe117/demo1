57b0c4f19fc3783799d0a3335bd85af4779edd5b
krzx <xyzthe117@gmail.com>
2020-04-26 18:22:28 +0700

    1

---
+++ app.js
@@ -0,0 +1,17 @@
+msg="hello";
+console.log(msg);
+console.log(Math.PI);
+function moreText(string){
+    return string+string;
+}
+console.log(moreText(msg))
+function logANumber(someNumber) {
+    console.log(someNumber)
+  }
+  for (i = 0, len = msg.length, text = ""; i < len; i++) { 
+    text += msg[i] + " ";
+  }
+  console.log(text);
+  var a=[0,1,2,3,4];
+  console.log(a[1]);
+  
